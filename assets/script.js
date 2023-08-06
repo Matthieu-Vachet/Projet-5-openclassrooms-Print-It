6307be4f603ab1b82df78c6924025edc6924ad55
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2023-08-06 21:44:43 +0200

    Add variable & addEventListener arrow

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,41 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"/assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"/assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"/assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"/assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+console.log(slides.length);
 
-const arrow_left = document.querySelector(".arrow_left");
+let arrow_left = document.querySelector(".arrow_left");
+let arrow_right = document.querySelector(".arrow_right");
+let banner_img = document.querySelector(".banner-img");
+let dot = document.querySelectorAll(".dot");
+let p = document.querySelector("#banner p");
+
+let currentSlide = 0;
+let totalslides = slides.length
+
+
 arrow_left.addEventListener("click", () => {
-	console.log("click sur le button gauche");
+	currentSlide = (currentSlide - 1 + totalslides) % totalslides;
 })
 
-const arrow_right = document.querySelector(".arrow_right");
 arrow_right.addEventListener("click", () => {
-	console.log("click sur le button droite");
+	currentSlide = (currentSlide + 1) % totalslides;
 })
+
+
+
+
